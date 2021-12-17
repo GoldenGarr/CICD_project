bd9ca463dfb3a5ad3d8c1abc03f7a84c294f4257
Maxim Tcai <89833124+GoldenGarr@users.noreply.github.com>
2021-12-17 23:08:49 +0300

    Created tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using testing::Eq;
+
+int wordcount(int argc, char *argv[3]);
+
+TEST(wordCountTest, empty_file) {
+    ASSERT_EQ(mult(2, 4), 8);
+}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
