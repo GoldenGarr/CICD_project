04b25302536d89ab4417c5ebd39218968bb731a9
Maxim Tcai <89833124+GoldenGarr@users.noreply.github.com>
2021-12-18 10:08:13 +0300

    lab1 test

--- tests.cpp
+++ tests.cpp
@@ -10,6 +10,15 @@ TEST(mult_test, some_values) {
     ASSERT_EQ(mult(0, 1), 0);
 }
 
+TEST(wordCountTest, two_lines) {
+    char *arr[3] = {"WordCount.exe", "-l", "threeLines.txt"};
+    ASSERT_EQ(wordcount(3, arr), 3);
+    char *arr1[3] = {"WordCount.exe", "-c", "threeLines.txt"};
+    ASSERT_EQ(wordcount(3, arr1), 72);
+    char *arr2[3] = {"WordCount.exe", "-w", "threeLines.txt"};
+    ASSERT_EQ(wordcount(3, arr2), 17);
+}
+
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
