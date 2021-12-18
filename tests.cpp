53e30e9f32c7111f1a7dd29179c86c2c6fe1a71b
Maxim Tcai <89833124+GoldenGarr@users.noreply.github.com>
2021-12-18 09:54:45 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -5,9 +5,9 @@ using testing::Eq;
 
 int wordcount(int argc, char *argv[3]);
 
-TEST(wordCountTest, empty_file) {
+TEST(mult_test, some_values) {
     ASSERT_EQ(mult(2, 4), 8);
-    ASSERT_EQ(mult(0, 1), 1);
+    ASSERT_EQ(mult(0, 1), 0);
 }
 
 int main(int argc, char** argv) {
